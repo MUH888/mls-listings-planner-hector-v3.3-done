4eb58888221c9408d5a46109748788b91d8ad626
Bryce St. Pierre <brycelstpierre@gmail.com>
2019-05-20 17:48:11 -0400

    Solved directions problem.

--- directions/script.js
+++ directions/script.js
@@ -1,5 +1,8 @@
-var map, markers, poly;
-var directionsService, directionsDisplay;
+var map, 
+    markers,
+    stopArray,
+    directionsService, 
+    directionsDisplay;
 
 var coords = [
   { lat: 41.879, lng: -87.624},
@@ -10,37 +13,26 @@ var coords = [
 ];
 
 function createMap () {
-  var options = {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 41.55, lng: -87.75 },
     mapTypeControl: false,
     rotateControl: false,
     zoom: 9
-  };
+  });
 
+  stopArray = new google.maps.MVCArray();
   directionsService = new google.maps.DirectionsService;
   directionsDisplay = new google.maps.DirectionsRenderer({ 
     preserveViewport: true
   });
 
-  map = new google.maps.Map(document.getElementById('map'), options);
+  var customControlDiv = document.createElement('div');
+  var customControl = new CustomControls(customControlDiv);
 
-  // Create the DIV to hold the control and call the CenterControl()
-  // constructor passing in this DIV.
-  var centerControlDiv = document.createElement('div');
-  var centerControl = new CenterControl(centerControlDiv);
-
-  centerControlDiv.index = 1;
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
-
-  directionsDisplay.setMap(map);
-
-  poly = new google.maps.Polyline({
-    strokeOpacity: 0
-  });
-  poly.setMap(map);
+  customControlDiv.index = 1;
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(customControlDiv);
 
   markers = [];
-
   markers = coords.map(function (c) {
     return new google.maps.Marker({
       position: c,
@@ -49,70 +41,103 @@ function createMap () {
   });
 
   markers.forEach(function (m) {
-    m.addListener('click', addToList);
+    m.addListener('click', function (e) {
+      stopArray.push(e.latLng);
+      updateDirections();
+    });
+  });
+
+  directionsDisplay.addListener('directions_changed', function () {
+    console.log('Directions changed.');
   });
 }
 
-function CenterControl(controlDiv) {
-
+function CustomControls (controlDiv) {
   // Set CSS for the control border.
-  var controlUI = document.createElement('div');
-  controlUI.style.backgroundColor = '#fff';
-  controlUI.style.border = '2px solid #fff';
-  controlUI.style.borderRadius = '3px';
-  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-  controlUI.style.cursor = 'pointer';
-  controlUI.style.marginTop = '8px';
-  controlUI.style.marginBottom = '22px';
-  controlUI.style.textAlign = 'center';
-  controlUI.title = 'Click to undo last stop.';
-  controlDiv.appendChild(controlUI);
+  var undoButton = document.createElement('div');
+  undoButton.style.backgroundColor = '#fff';
+  undoButton.style.border = '2px solid #fff';
+  undoButton.style.borderRadius = '3px';
+  undoButton.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  undoButton.style.cursor = 'pointer';
+  undoButton.style.marginTop = '10px';
+  undoButton.style.marginLeft = '10px';
+  undoButton.style.marginBottom = '10px';
+  undoButton.style.textAlign = 'center';
+  undoButton.title = 'Click to undo last stop.';
+  controlDiv.appendChild(undoButton);
 
   // Set CSS for the control interior.
-  var controlText = document.createElement('div');
-  controlText.style.color = 'rgb(25,25,25)';
-  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-  controlText.style.fontSize = '16px';
-  controlText.style.lineHeight = '38px';
-  controlText.style.paddingLeft = '5px';
-  controlText.style.paddingRight = '5px';
-  controlText.innerHTML = 'Undo Last Stop';
-  controlUI.appendChild(controlText);
+  var undoText = document.createElement('div');
+  undoText.style.color = 'rgb(25,25,25)';
+  undoText.style.fontFamily = 'Roboto,Arial,sans-serif';
+  undoText.style.fontSize = '16px';
+  undoText.style.lineHeight = '38px';
+  undoText.style.paddingLeft = '5px';
+  undoText.style.paddingRight = '5px';
+  undoText.innerHTML = 'Undo Last Stop';
+  undoButton.appendChild(undoText);
 
-  // Setup the click event listeners: simply set the map to Chicago.
-  controlUI.addEventListener('click', function() {
-    var newPath = poly.getPath();
-    newPath.pop();
-    poly.setPath(newPath);
-    updateDirections(poly.getPath());
+  // Set CSS for the control border.
+  var resetButton = document.createElement('div');
+  resetButton.style.backgroundColor = '#fff';
+  resetButton.style.border = '2px solid #fff';
+  resetButton.style.borderRadius = '3px';
+  resetButton.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  resetButton.style.cursor = 'pointer';
+  resetButton.style.marginTop = '10px';
+  resetButton.style.marginLeft = '10px';
+  resetButton.style.marginBottom = '22px';
+  resetButton.style.textAlign = 'center';
+  resetButton.title = 'Click to reset directions.';
+  controlDiv.appendChild(resetButton);
+
+  // Set CSS for the control interior.
+  var resetText = document.createElement('div');
+  resetText.style.color = 'rgb(25,25,25)';
+  resetText.style.fontFamily = 'Roboto,Arial,sans-serif';
+  resetText.style.fontSize = '16px';
+  resetText.style.lineHeight = '38px';
+  resetText.style.paddingLeft = '5px';
+  resetText.style.paddingRight = '5px';
+  resetText.innerHTML = 'Reset Directions';
+  resetButton.appendChild(resetText);
+
+  undoButton.addEventListener('click', function () {
+    stopArray.pop();
+    if (stopArray.length === 1) // Pop again if only one stop remains.
+      stopArray.pop();
+    updateDirections();
+  });
+
+  resetButton.addEventListener('click', function () {
+    stopArray.clear();
+    updateDirections();
   });
 }
 
-function addToList (event) {
-  var path = poly.getPath();
-  path.push(event.latLng);
+function updateDirections () {
 
-  updateDirections(path);
-}
-
-function updateDirections (path) {
-  if (path.getLength() <= 1)
+  if (stopArray.getLength() <= 1) {
+    directionsDisplay.setMap(null);
     return;
-
+  }
   var waypoints = [];
 
-  path.forEach(function (e, i) {
-    if (i !== 0 && i !== path.getLength() - 1) {
+  stopArray.forEach(function (stop, i) {
+    if (i !== 0 && i !== stopArray.getLength() - 1) {
       waypoints.push({
-        location: path.getAt(i),
+        location: stop,
         stopover: true
       });
     }
   });
 
+  directionsDisplay.setMap(map);
+
   directionsService.route({
-    origin: path.getAt(0),
-    destination: path.getAt(path.getLength() - 1),
+    origin: stopArray.getAt(0),
+    destination: stopArray.getAt(stopArray.getLength() - 1),
     waypoints: waypoints,
     optimizeWaypoints: true,
     travelMode: 'DRIVING'
